022373e2324e4b183798d7f339b7167fa52fbc5e
Imthath <imthath.m@hotmail.com>
2021-05-19 06:13:06 +0530

    load verses into CoreData from array

--- Package.swift
+++ Package.swift
@@ -4,25 +4,36 @@
 import PackageDescription
 
 let package = Package(
-    name: "QuranKit",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "QuranKit",
-            targets: ["QuranKit"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "QuranKit",
-            dependencies: []),
-        .testTarget(
-            name: "QuranKitTests",
-            dependencies: ["QuranKit"]),
-    ]
+	name: "QuranKit",
+	platforms: [.iOS(.v13), .watchOS(.v6), .macOS(.v10_15)],
+	products: [
+		// Products define the executables and libraries a package produces, and make them visible to other packages.
+		.library(
+			name: "QuranKit",
+			targets: ["QuranKit"]
+		),
+	],
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+		// .package(url: /* package url */, from: "1.0.0"),
+//			.package(
+//				name: "GRDB",
+//				url: "https://github.com/groue/GRDB.swift",
+//				.exact("5.7.4-sqlcipher")
+//			)
+		.package(path: "~/Desktop/Frameworks/MILocalStore"),
+	],
+	targets: [
+		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+		// Targets can depend on other targets in this package, and on products in packages this package depends on.
+		.target(
+			name: "QuranKit",
+			dependencies: [.product(name: "MILocalStore", package: "MILocalStore")],
+			resources: [.copy("Resources/CD_Quran.sqlite")]
+		),
+		.testTarget(
+			name: "QuranKitTests",
+			dependencies: ["QuranKit"]
+		),
+	]
 )
--- Sources/QuranKit/QuranKit.swift
+++ Sources/QuranKit/QuranKit.swift
@@ -1,3 +1,63 @@
-struct QuranKit {
-    var text = "Hello, World!"
+import MILocalStore
+import CoreData
+
+var QuranKitBundle: Bundle { Bundle.module }
+
+public class QuranStore: CoreDataStorable {
+	private init() {
+		self.persistantContainer = QuranStore.makeContainer()
+	}
+
+	open var modelName: String { "Verses "}
+
+	open var persistantContainer: NSPersistentContainer
+
+	open lazy var mainContext: NSManagedObjectContext = persistantContainer.viewContext
+
+	open lazy var privateContextWithParent: NSManagedObjectContext = mainContext.privateChildContext
+
+	public static let shared = QuranStore()
+
+	public static var context: NSManagedObjectContext { shared.currentContext }
+
+	public static func save() { shared.saveChanges() }
+
+	static func makeContainer() -> NSPersistentContainer {
+		let url = Bundle.module.url(forResource: "Verses", withExtension: "momd")
+		let model = NSManagedObjectModel(contentsOf: url!)
+		let container: NSPersistentCloudKitContainer = .init(name: "Verses", managedObjectModel: model!)
+		container.loadPersistentStores { storeDesc, error in
+			if let error = error {
+				assertionFailure(error.localizedDescription)
+			}
+
+			if storeDesc.url == nil {
+				assertionFailure("No Store URL")
+			}
+
+			print("URL - " + storeDesc.url!.absoluteString)
+		}
+		return container
+	}
+
+	public static var verses: [CDVerse] {
+		let fetchRequest: NSFetchRequest<CDVerse> = CDVerse.fetchRequest()
+
+		return try! context.fetch(fetchRequest)
+	}
+
+	public func loadVerses() {
+		guard Self.verses.isEmpty else {
+			return
+		}
+
+		ALL_VERSES.forEach {
+			let verse = CDVerse(context: QuranStore.context)
+			verse.ayah = Int16($0.ayah)
+			verse.surah = Int16($0.sura)
+			verse.text = $0.text
+		}
+
+		QuranStore.save()
+	}
 }
