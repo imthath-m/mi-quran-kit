a4d6858d0a28f592e2c0115bc30f193ee4dce7f9
Imthath <imthath.m@hotmail.com>
2021-05-29 15:57:12 +0530

    Add methods to get verse in a range or a surah. Add empty check.

--- Sources/QuranKit/QuranKit.swift
+++ Sources/QuranKit/QuranKit.swift
@@ -4,8 +4,27 @@ import CoreData
 var QuranKitBundle: Bundle { Bundle.module }
 
 public class QuranStore: CoreDataStorable {
-	private init() {
-		self.persistantContainer = QuranStore.makeContainer()
+
+  public static var preview: QuranStore = {
+    let store = QuranStore(loadstoreByDefault: false)
+    store.persistantContainer.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+    store.persistantContainer.loadPersistentStores { storeDesc, error in
+      if let error = error {
+        assertionFailure(error.localizedDescription)
+      }
+
+      if let url = storeDesc.url {
+        print("URL - " + url.absoluteString)
+      } else {
+        assertionFailure("No Store URL")
+      }
+    }
+
+    return store
+  }()
+
+  private init(loadstoreByDefault: Bool = true) {
+    self.persistantContainer = QuranStore.makeContainer(loadstoreByDefault: loadstoreByDefault)
 	}
 
 	open var modelName: String { "Verses "}
@@ -22,10 +41,13 @@ public class QuranStore: CoreDataStorable {
 
 	public static func save() { shared.saveChanges() }
 
-	static func makeContainer() -> NSPersistentContainer {
+	static func makeContainer(loadstoreByDefault: Bool = true) -> NSPersistentContainer {
 		let url = Bundle.module.url(forResource: "Verses", withExtension: "momd")
 		let model = NSManagedObjectModel(contentsOf: url!)
 		let container: NSPersistentCloudKitContainer = .init(name: "Verses", managedObjectModel: model!)
+    guard loadstoreByDefault else {
+      return container
+    }
 		container.loadPersistentStores { storeDesc, error in
 			if let error = error {
 				assertionFailure(error.localizedDescription)
@@ -40,14 +62,19 @@ public class QuranStore: CoreDataStorable {
 		return container
 	}
 
-	public static var verses: [CDVerse] {
-		let fetchRequest: NSFetchRequest<CDVerse> = CDVerse.fetchRequest()
-
-		return try! context.fetch(fetchRequest)
-	}
+  private static var isEmpty: Bool {
+    let fetchRequest: NSFetchRequest<CDVerse> = CDVerse.fetchRequest()
+    fetchRequest.fetchLimit = 1
+    do {
+      return try context.fetch(fetchRequest).isEmpty
+    } catch {
+      assertionFailure(error.localizedDescription)
+      return true
+    }
+  }
 
 	public func loadVerses() {
-		guard Self.verses.isEmpty else {
+		guard Self.isEmpty else {
 			return
 		}
 
@@ -61,7 +88,63 @@ public class QuranStore: CoreDataStorable {
 		QuranStore.save()
 	}
 
+  public static func verses(from source: VerseSource) -> [CDVerse] {
+    switch source {
+    case .fullSurah(let surah): return verses(in: surah)
+    case .range(let range): return verses(in: range)
+    }
+  }
+
+  public static func versesCount(from source: VerseSource) -> Int16 {
+    Int16(verses(from: source).count)
+  }
+
+  private static func verses(in surah: Int16) -> [CDVerse] {
+    let fetchRequest: NSFetchRequest<CDVerse> = CDVerse.fetchRequest()
+    fetchRequest.predicate = NSPredicate(format: "surah = %d", surah)
+    fetchRequest.sortDescriptors = [
+      NSSortDescriptor(key: "ayah", ascending: true)
+    ]
+    do {
+      return try context.fetch(fetchRequest)
+    } catch {
+      assertionFailure(error.localizedDescription)
+      return []
+    }
+  }
+
+  private static func verses(in range: VersesRange) -> [CDVerse] {
+    let fetchRequest: NSFetchRequest<CDVerse> = CDVerse.fetchRequest()
+    fetchRequest.predicate = NSPredicate(
+      format: "surah >= %d AND surah <= %d",
+      range.startSurah, range.endSurah
+    )
+    fetchRequest.sortDescriptors = [
+      NSSortDescriptor(key: "surah", ascending: true),
+      NSSortDescriptor(key: "ayah", ascending: true)
+    ]
+    do {
+      return try context.fetch(fetchRequest).filter { verse in
+        if verse.surah == range.startSurah,
+           verse.ayah < range.startAyah {
+          return false
+        }
+
+        if verse.surah == range.endSurah,
+           verse.ayah > range.endAyah {
+          return false
+        }
+
+        return true
+      }
+    } catch {
+      assertionFailure(error.localizedDescription)
+      return []
+    }
+  }
+
   public static func surahName(for number: Int16, in language: Language) -> String {
+    guard number > 0, number <= 114 else { return "" }
     switch language {
     case .english: return english[Int(number-1)]
     case .arabic: return arabic[Int(number-1)]
@@ -73,5 +156,24 @@ public class QuranStore: CoreDataStorable {
 
   public enum Language {
     case english, arabic
+  }
+}
+
+public enum VerseSource {
+  case fullSurah(Int16)
+  case range(VersesRange)
+}
+
+public struct VersesRange {
+  var startSurah: Int16
+  var startAyah: Int16
+  var endSurah: Int16
+  var endAyah: Int16
+
+  public init(startSurah: Int16, startAyah: Int16, endSurah: Int16, endAyah: Int16) {
+    self.startSurah = startSurah
+    self.startAyah = startAyah
+    self.endSurah = endSurah
+    self.endAyah = endAyah
   }
 }
--- Sources/QuranKit/SurahNames/SurahsEnglish.swift
+++ Sources/QuranKit/SurahNames/SurahsEnglish.swift
@@ -8,118 +8,118 @@
 import Foundation
 
 class SurahsEnglish: SurahName {
-  var surah0: String { "Al-Fatihah" }
-  var surah1: String { "Al-Baqarah" }
-  var surah2: String { "Aal-E-Imran" }
-  var surah3: String { "An-Nisa'" }
-  var surah4: String { "Al-Ma'idah" }
-  var surah5: String { "Al-An'am" }
-  var surah6: String { "Al-A'raf" }
-  var surah7: String { "Al-Anfal" }
-  var surah8: String { "At-Tawbah" }
+  var surah0: String { "Al Fatihah" }
+  var surah1: String { "Al Baqarah" }
+  var surah2: String { "Aal E Imran" }
+  var surah3: String { "An Nisa'" }
+  var surah4: String { "Al Ma'idah" }
+  var surah5: String { "Al An'am" }
+  var surah6: String { "Al A'raf" }
+  var surah7: String { "Al Anfal" }
+  var surah8: String { "At Tawbah" }
   var surah9: String { "Yunus" }
   var surah10: String { "Hud" }
   var surah11: String { "Yusuf" }
-  var surah12: String { "Ar-Ra'd" }
+  var surah12: String { "Ar Ra'd" }
   var surah13: String { "Ibrahim" }
-  var surah14: String { "Al-Hijr" }
-  var surah15: String { "An-Nahl" }
-  var surah16: String { "Al-Isra'" }
-  var surah17: String { "Al-Kahf" }
+  var surah14: String { "Al Hijr" }
+  var surah15: String { "An Nahl" }
+  var surah16: String { "Al Isra'" }
+  var surah17: String { "Al Kahf" }
   var surah18: String { "Maryam" }
-  var surah19: String { "Ta-Ha" }
-  var surah20: String { "Al-Anbiya'" }
-  var surah21: String { "Al-Hajj" }
-  var surah22: String { "Al-Mu'minun" }
-  var surah23: String { "An-Nur" }
-  var surah24: String { "Al-Furqan" }
-  var surah25: String { "Ash-Shu'ara" }
-  var surah26: String { "An-Naml" }
-  var surah27: String { "Al-Qasas" }
-  var surah28: String { "Al-Ankabut" }
-  var surah29: String { "Ar-Rum" }
+  var surah19: String { "Ta Ha" }
+  var surah20: String { "Al Anbiya'" }
+  var surah21: String { "Al Hajj" }
+  var surah22: String { "Al Mu'minun" }
+  var surah23: String { "An Nur" }
+  var surah24: String { "Al Furqan" }
+  var surah25: String { "Ash Shu'ara" }
+  var surah26: String { "An Naml" }
+  var surah27: String { "Al Qasas" }
+  var surah28: String { "Al Ankabut" }
+  var surah29: String { "Ar Rum" }
   var surah30: String { "Luqman" }
-  var surah31: String { "As-Sajdah" }
-  var surah32: String { "Al-Ahzab" }
+  var surah31: String { "As Sajdah" }
+  var surah32: String { "Al Ahzab" }
   var surah33: String { "Saba'" }
   var surah34: String { "Fatir" }
-  var surah35: String { "Ya-Seen" }
-  var surah36: String { "As-Saaffat" }
+  var surah35: String { "Ya Seen" }
+  var surah36: String { "As Saaffat" }
   var surah37: String { "Sad" }
-  var surah38: String { "Az-Zumar" }
+  var surah38: String { "Az Zumar" }
   var surah39: String { "Ghafir" }
   var surah40: String { "Fussilat" }
-  var surah41: String { "Ash-Shura" }
-  var surah42: String { "Az-Zukhruf" }
-  var surah43: String { "Ad-Dukhan" }
-  var surah44: String { "Al-Jathiya" }
-  var surah45: String { "Al-Ahqaf" }
+  var surah41: String { "Ash Shura" }
+  var surah42: String { "Az Zukhruf" }
+  var surah43: String { "Ad Dukhan" }
+  var surah44: String { "Al Jathiya" }
+  var surah45: String { "Al Ahqaf" }
   var surah46: String { "Muhammad" }
-  var surah47: String { "Al-Fath" }
-  var surah48: String { "Al-Hujurat" }
+  var surah47: String { "Al Fath" }
+  var surah48: String { "Al Hujurat" }
   var surah49: String { "Qaf" }
-  var surah50: String { "Adh-Dhariyat" }
-  var surah51: String { "At-Tur" }
-  var surah52: String { "An-Najm" }
-  var surah53: String { "Al-Qamar" }
-  var surah54: String { "Ar-Rahman" }
-  var surah55: String { "Al-Waqi'ah" }
-  var surah56: String { "Al-Hadid" }
-  var surah57: String { "Al-Mujadila" }
-  var surah58: String { "Al-Hashr" }
-  var surah59: String { "Al-Mumtahana" }
-  var surah60: String { "As-Saf" }
-  var surah61: String { "Al-Jumu'ah" }
-  var surah62: String { "Al-Munafiqun" }
-  var surah63: String { "At-Taghabun" }
-  var surah64: String { "At-Talaq" }
-  var surah65: String { "At-Tahrim" }
-  var surah66: String { "Al-Mulk" }
-  var surah67: String { "Al-Qalam" }
-  var surah68: String { "Al-Haqqah" }
-  var surah69: String { "Al-Ma'arij" }
+  var surah50: String { "Adh Dhariyat" }
+  var surah51: String { "At Tur" }
+  var surah52: String { "An Najm" }
+  var surah53: String { "Al Qamar" }
+  var surah54: String { "Ar Rahman" }
+  var surah55: String { "Al Waqi'ah" }
+  var surah56: String { "Al Hadid" }
+  var surah57: String { "Al Mujadila" }
+  var surah58: String { "Al Hashr" }
+  var surah59: String { "Al Mumtahana" }
+  var surah60: String { "As Saf" }
+  var surah61: String { "Al Jumu'ah" }
+  var surah62: String { "Al Munafiqun" }
+  var surah63: String { "At Taghabun" }
+  var surah64: String { "At Talaq" }
+  var surah65: String { "At Tahrim" }
+  var surah66: String { "Al Mulk" }
+  var surah67: String { "Al Qalam" }
+  var surah68: String { "Al Haqqah" }
+  var surah69: String { "Al Ma'arij" }
   var surah70: String { "Nuh" }
-  var surah71: String { "Al-Jinn" }
-  var surah72: String { "Al-Muzzammil" }
-  var surah73: String { "Al-Muddaththir" }
-  var surah74: String { "Al-Qiyamah" }
-  var surah75: String { "Al-Insan" }
-  var surah76: String { "Al-Mursalat" }
-  var surah77: String { "An-Naba'" }
-  var surah78: String { "An-Nazi'at" }
+  var surah71: String { "Al Jinn" }
+  var surah72: String { "Al Muzzammil" }
+  var surah73: String { "Al Muddaththir" }
+  var surah74: String { "Al Qiyamah" }
+  var surah75: String { "Al Insan" }
+  var surah76: String { "Al Mursalat" }
+  var surah77: String { "An Naba'" }
+  var surah78: String { "An Nazi'at" }
   var surah79: String { "'Abasa" }
-  var surah80: String { "At-Takwir" }
-  var surah81: String { "Al-Infitar" }
-  var surah82: String { "Al-Mutaffifin" }
-  var surah83: String { "Al-Inshiqaq" }
-  var surah84: String { "Al-Buruj" }
-  var surah85: String { "At-Tariq" }
-  var surah86: String { "Al-A'la" }
-  var surah87: String { "Al-Ghashiyah" }
-  var surah88: String { "Al-Fajr" }
-  var surah89: String { "Al-Balad" }
-  var surah90: String { "Ash-Shams" }
-  var surah91: String { "Al-Layl" }
-  var surah92: String { "Ad-Dhuhaa" }
-  var surah93: String { "Al-Sharh" }
-  var surah94: String { "At-Tin" }
-  var surah95: String { "Al-Alaq" }
-  var surah96: String { "Al-Qadr" }
-  var surah97: String { "Al-Bayyinah" }
-  var surah98: String { "Az-Zalzalah" }
-  var surah99: String { "Al-Adiyat" }
-  var surah100: String { "Al-Qari'ah" }
-  var surah101: String { "At-Takathur" }
-  var surah102: String { "Al-Asr" }
-  var surah103: String { "Al-Humazah" }
-  var surah104: String { "Al-Fil" }
+  var surah80: String { "At Takwir" }
+  var surah81: String { "Al Infitar" }
+  var surah82: String { "Al Mutaffifin" }
+  var surah83: String { "Al Inshiqaq" }
+  var surah84: String { "Al Buruj" }
+  var surah85: String { "At Tariq" }
+  var surah86: String { "Al A'la" }
+  var surah87: String { "Al Ghashiyah" }
+  var surah88: String { "Al Fajr" }
+  var surah89: String { "Al Balad" }
+  var surah90: String { "Ash Shams" }
+  var surah91: String { "Al Layl" }
+  var surah92: String { "Ad Dhuhaa" }
+  var surah93: String { "Al Sharh" }
+  var surah94: String { "At Tin" }
+  var surah95: String { "Al Alaq" }
+  var surah96: String { "Al Qadr" }
+  var surah97: String { "Al Bayyinah" }
+  var surah98: String { "Az Zalzalah" }
+  var surah99: String { "Al Adiyat" }
+  var surah100: String { "Al Qari'ah" }
+  var surah101: String { "At Takathur" }
+  var surah102: String { "Al Asr" }
+  var surah103: String { "Al Humazah" }
+  var surah104: String { "Al Fil" }
   var surah105: String { "Quraysh" }
-  var surah106: String { "Al-Ma'un" }
-  var surah107: String { "Al-Kawthar" }
-  var surah108: String { "Al-Kafirun" }
-  var surah109: String { "An-Nasr" }
-  var surah110: String { "Al-Masad" }
-  var surah111: String { "Al-Ikhlas" }
-  var surah112: String { "Al-Falaq" }
-  var surah113: String { "An-Nas" }
+  var surah106: String { "Al Ma'un" }
+  var surah107: String { "Al Kawthar" }
+  var surah108: String { "Al Kafirun" }
+  var surah109: String { "An Nasr" }
+  var surah110: String { "Al Masad" }
+  var surah111: String { "Al Ikhlas" }
+  var surah112: String { "Al Falaq" }
+  var surah113: String { "An Nas" }
 }
